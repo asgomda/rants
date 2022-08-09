ccd72394a8f1b913125015632645329c8520126d
Abdul Samad Gomda <ag7766@nyu.edu>
2022-08-09 18:16:11 +0000

    ✅Added rant model

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const router = express.Router();
-
+const Rant = require('../models/Rant')
 const {ensureAuthenticated, ensureGuest} = require('../middleware/auth')
 
 // @desc Login/Landing page
@@ -14,8 +14,19 @@ router.get('/', ensureGuest, (req, res) => {
 
 // @desc dashboard page
 //@route /dashboard
-router.get('/dashboard', ensureAuthenticated, (req, res) => {
-    res.render('dashboard')
+router.get('/dashboard', ensureAuthenticated, async (req, res) => {
+    try {
+        // finding stories in dashboard
+        const rants = await Rant.find({user: req.user.id})
+        res.render('dashboard', {
+            name: req.user.firstName,
+            rants
+        })
+    } catch (error) {
+        console.log(error)
+    }
+    
+
 })
 
 
