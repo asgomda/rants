ddb82212b779cda35182d9f888e1f0ba5026060e
Abdul Samad Gomda <ag7766@nyu.edu>
2022-08-10 11:44:46 +0000

    🐯added public stories page

--- routes/rants.js
+++ routes/rants.js
@@ -5,18 +5,35 @@ const Rant = require('../models/Rant')
 const {ensureAuthenticated} = require('../middleware/auth')
 
 // @desc add rant page
-//@route /rants/add
+//@route GET /rants/add
 router.get('/add', ensureAuthenticated, (req, res) => {
     res.render('rants/add')
 })
 
 // @desc parse the form data
-//@route /rants/
+//@route POST /rants/
 router.post('/', ensureAuthenticated, async (req, res) => {
     try {
         req.body.user = req.user; // adding the user to the body for the rant model
         await Rant.create(req.body)
         res.redirect('/dashboard')
+    }catch (err) {
+        console.log(err)
+        res.render('errors/500')
+    }
+})
+
+// @desc populate public rants
+//@route GET /rants
+router.get('/', ensureAuthenticated, async (req, res) => {
+    try {
+        const rants = await Rant.find({status: 'public'})
+                            .populate('user')
+                            .sort({createdAt: 'desc'})
+                            .lean()
+        res.render('rants/index', {
+            rants,
+        })
     }catch (err) {
         console.log(err)
         res.render('errors/500')
